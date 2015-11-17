6db6fa2faeb4952411a65bbd62f58b8d9717f875
nickhillwd <nickhillwd@gmail.com>
2015-11-17 16:34:12 +0000

    Initial commit from classwork day 2 on functions

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,98 @@
+def return_10
+  10
+end
+
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def multiply(a, b)
+  a * b
+end
+
+def divide(a, b)
+  a / b
+end
+
+# String Play
+
+def length_of_string(string)
+  string.length
+end  
+
+def join_string(string1, string2)
+  string1 + " " + string2
+end
+
+# Type conversion
+
+def add_string_as_number(a, b)
+  a.to_i + b.to_i
+end
+
+# Conditional logic
+
+def number_to_full_month_name(month)
+  case month
+  when 1
+    "January"
+  when 3
+    "March"
+  when 9
+    "September"
+  end
+end
+
+def number_to_short_month_name(short_month)
+  case short_month
+  when 1
+    "Jan"
+  when 3
+    "Mar"
+  when 9
+    "Sep"
+  end
+end
+
+
+def cube_vol(length_unit)
+  length_unit ** 3
+end
+
+def sphere_volume(length_unit)
+  (4*(Math::PI)*(length_unit**3))/3
+end
+
+#------------------------------------------------------------------------#
+
+def todays_day_in_year
+  Time.now.strftime("%j")
+end
+
+def nights_to_xmas
+  358 - todays_day_in_year.to_i
+end
+
+#-----------------------------------------------------------------------#
+
+date_of_birth = gets.chomp.tr('/', '').to_i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
